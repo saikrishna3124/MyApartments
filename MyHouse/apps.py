bea714b9fd006fd42b9abbe6a1a2f7604b9590c9
CHALLA Sai <sai.challa@hexagon.com>
2020-06-01 17:43:08 +0530

    Initial Code Commit

---
+++ MyHouse/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MyhouseConfig(AppConfig):
+    name = 'MyHouse'
---
+++ MyHouse/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    #dests= Destination.objects.all()
+    return render(request,'index.html')
